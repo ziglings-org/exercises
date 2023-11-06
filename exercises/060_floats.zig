55b58fea56b1ae9b85d5fafe99a4194112da7da3
Chris Boesch <chrboesch@noreply.codeberg.org>
2023-11-06 19:50:47 +0100

    Change the task so that the exercise contains two errors again.

--- exercises/060_floats.zig
+++ exercises/060_floats.zig
@@ -39,11 +39,11 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     // The approximate weight of the Space Shuttle upon liftoff
-    // (including boosters and fuel tank) was 2,200 tons.
+    // (including boosters and fuel tank) was 4,480,000 lb.
     //
-    // We'll convert this weight from tons to kilograms at a
-    // conversion of 907.18kg to the ton.
-    const shuttle_weight: f16 = 907.18 * 2200;
+    // We'll convert this weight from pound to kilograms at a
+    // conversion of 0.453592kg to the pound.
+    const shuttle_weight: f16 = 0.453592 * 4480e6;
 
     // By default, float values are formatted in scientific
     // notation. Try experimenting with '{d}' and '{d:.3}' to see
