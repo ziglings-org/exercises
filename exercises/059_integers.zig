d0519d18fade9d099f4737f4e4c6ade976c2a685
Chris Boesch <chrboesch@noreply.codeberg.org>
2024-03-08 01:07:57 +0100

    Fixed unicode literal

--- exercises/059_integers.zig
+++ exercises/059_integers.zig
@@ -2,12 +2,12 @@
 // Zig lets you express integer literals in several convenient
 // formats. These are all the same value:
 //
-//     const a1: u8 = 65;        // decimal
-//     const a2: u8 = 0x41;      // hexadecimal
-//     const a3: u8 = 0o101;     // octal
-//     const a4: u8 = 0b1000001; // binary
-//     const a5: u8 = 'A';       // ASCII code point literal
-//     const a6: u16 = 'Ȁ';      // Unicode code points can take up to 21 bits
+//     const a1: u8 = 65;          // decimal
+//     const a2: u8 = 0x41;        // hexadecimal
+//     const a3: u8 = 0o101;       // octal
+//     const a4: u8 = 0b1000001;   // binary
+//     const a5: u8 = 'A';         // ASCII code point literal
+//     const a6: u16 = '\u{0041}'; // Unicode code points can take up to 21 bits
 //
 // You can also place underscores in numbers to aid readability:
 //
